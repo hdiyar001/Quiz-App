9b5737a414906390afa1b34d83b23a2370fb9bfe
janalo <janalo957@gmail.com>
2023-11-27 14:49:20 +0100

    color changes

--- src/Components/AnswerSection.jsx
+++ src/Components/AnswerSection.jsx
@@ -1,33 +1,45 @@
-import styles from './AnswerSection.module.css';
-import React, { useEffect, useState } from 'react';
-
+import styles from "./AnswerSection.module.css";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export function AnswerSection(props) {
-    const [phase, setPhase] = useState(false);
+ 
 
-    const StyledDiv = styled.div`
-    background-color: #8db1fe;
-    color: white;
+  const StyledDiv = styled.div`
+    background-color: ${props.Color};
     cursor: pointer;
-  
-    &:hover {
-    background-color: #834ae2;
-    color: white;
-    }
-  
-    &:active {
-    background-color: #e59b66;
     color: black;
+    border: none;
+    height: 45px;
+    border-radius: 30px;
+    margin-top: 8px;
+    margin: 8px 45px;
+    padding: 5px 0px;
+
+    &:hover {
+      background-color: #834ae2;
+      color: white;
+    }
+
+    &:active {
+      background-color: #e59b66;
+      color: black;
+    }
+
+    p {
+      font-size: 14px;
     }
   `;
 
-    function redirectOnclickHandler() {
-        setIsClicked(true)
-        props.handleOnlick(70);
-    }
-    return (
-        <div className={styles.answer_section} style={StyledDiv} onClick={redirectOnclickHandler} >
-            <p>{props.answer.text}</p>
-        </div >
-    )
+  function redirectOnclickHandler() {
+    props.handleOnlick();
+  }
+  return (
+    <StyledDiv
+      className={styles.answer_section}
+      onClick={redirectOnclickHandler}
+    >
+      <p>{props.answer.text}</p>
+    </StyledDiv>
+  );
 }
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -6,6 +6,8 @@ const maxInterval = 60;
 export function Quiz({ question }) {
     const [timer, setTimer] = useState(maxInterval);
     const [sleep, setSleep] = useState(1000);
+    const [phase, setPhase] = useState(0); // 0 before answering, analysing the answer 1, Results 2 
+    const [answerSectionColor, setanswerSectionColor] = useState("#8db1fe");
     // const [isQuestionDone, setIsQuestionDone] = useState(0);
 
     useEffect(() => {
@@ -14,12 +16,22 @@ export function Quiz({ question }) {
                 setTimer(maxInterval);
                 setSleep(1000);
             } else {
+                switch(phase) {
+                    case 0 : setSleep(1000); setanswerSectionColor("#8db1fe");
+                    break;
+                    case 1 : setSleep(70); setanswerSectionColor("#e59b66");
+                    break;
+                    case 2 : setSleep(70); setanswerSectionColor("red");
+                    break;
+                }
                 setTimer(timer => (timer - 1));
             }
         }, sleep);
 
         return () => clearInterval(interval);
-    }, [timer]);
+    }, [phase,timer]);
+
+        console.log(phase);
 
     return (
         <div className={classes.quiz_container}>
@@ -27,8 +39,9 @@ export function Quiz({ question }) {
             <progress value={timer} max={maxInterval}></progress>
             <h2 > {question.question}</h2>
 
-            {question.answers.map((answer, key) => < AnswerSection key={key} answer={answer}
-                handleOnlick={(sleep) => { setSleep(sleep); setTimer(maxInterval) }}
+            {question.answers.map((answer, key) => < AnswerSection key={key} answer={answer} Color={answerSectionColor}
+                handleOnlick={() => { setPhase(phase+1); setTimer(maxInterval) }}
+            
             />)}
         </div >
     );
